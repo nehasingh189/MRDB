a156ac49de1b32ffdc87df585dd741bf9b0d36a6
Neha singh <nehasingh18.9@gmail.com>
2016-12-15 13:31:11 -0500

    heroku changes

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ var mongoose      = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/cs5610spring2016exmpl1';
 
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
 var db = mongoose.connect(connectionString);
 
 app.use(bodyParser.json());// for parsing application/json
